d96dd4955d8747d1edb0a6bc3f1fd27cecde4695
Shadreck Khamba <bsc-inf-21-19@unima.ac.mw>
2022-01-16 13:17:51 -0800

    initial commit project setup

---
+++ src/main/java/com/example/demo/MyWebApplication/ApplicationRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo.MyWebApplication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ApplicationRepository
+        extends JpaRepository<MyWebApplication, Long> {
+    @Query("SELECT s FROM MyWebApplication s WHERE s.name = ?1")
+    Optional<MyWebApplication>findMyWebApplicationByName(String name);
+    @Query("SELECT s FROM MyWebApplication s WHERE s.email=?1")
+    <myWebApplication> Optional<myWebApplication> findMyWebApplicationByEmail(String email);
+
+ // Optional<Object> findByName(String myWebApplicationName);
+public List<MyWebApplication> findByName(String name);
+
+
+
+
+}
